c3a45e6ef6fea735ae1bc52ded505791f7430461
Volcomix <volcomix@gmail.com>
2017-06-12 13:35:29 +0200

    Remove backgroundColor from styles.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { backgroundColor } from './shared/styles.js'
 import Beat from './core/Beat.component'
 import BassDrum from './synth/BassDrum/BassDrum.component'
 import BassDrumSynth from './synth/BassDrum/BassDrum'
@@ -8,10 +9,16 @@ const context = new AudioContext()
 const bassDrumSynth = new BassDrumSynth(context, context.destination)
 
 const App = () => (
-  <div>
+  <div style={styles.container}>
     <Beat context={context} synth={bassDrumSynth} />
     <BassDrum synth={bassDrumSynth} />
   </div>
 )
+
+const styles = {
+  container: {
+    backgroundColor,
+  },
+}
 
 export default App
