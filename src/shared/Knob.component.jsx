a604a39e0aa96a835a195ec5fe467cd9b823ef80
Volcomix <volcomix@gmail.com>
2017-06-14 23:38:38 +0200

    Init knob background drawing

--- src/shared/Knob.component.jsx
+++ src/shared/Knob.component.jsx
@@ -1,27 +1,50 @@
 import React from 'react'
 
+import { keyColor, borderColor } from './styles'
+
+const minAngle = 0.75 * Math.PI
+const maxAngle = 0.25 * Math.PI
+const selectableAngle = 2 * Math.PI - minAngle + maxAngle
+const width = 24
+const lineWidth = 1.5
+
 class Knob extends React.Component {
   static defaultProps = { min: 0 }
 
+  componentDidMount() {
+    this.drawBackground()
+  }
+
+  componentDidUpdate() {
+    this.draw()
+  }
+
   render() {
-    const { label, min, max, step, value } = this.props
+    const { label, min, max, step, value, onValueChange } = this.props
     return (
       <span>
-        <label>{label}</label>
-        <input
-          type='range'
-          min={min}
-          max={max}
-          step={step}
-          value={value}
-          onChange={this.handleInputChange}
+        <canvas
+          width={width}
+          height={width}
+          ref={canvas => this.background = canvas}
         />
       </span>
     )
   }
 
-  handleInputChange = event => {
-    this.props.onValueChange(event.target.valueAsNumber)
+  drawBackground() {
+    const center = width / 2
+    const radius = center - lineWidth / 2
+
+    const ctx = this.background.getContext('2d')
+    ctx.lineWidth = lineWidth
+    ctx.strokeStyle = borderColor
+    ctx.beginPath()
+    ctx.arc(center, center, radius, minAngle, maxAngle)
+    ctx.stroke()
+  }
+
+  draw() {
   }
 }
 
