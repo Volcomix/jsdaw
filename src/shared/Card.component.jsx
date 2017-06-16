f1ccff739c9fafd91d0aed2db7ec3a840595b2e6
Volcomix <volcomix@gmail.com>
2017-06-16 19:11:55 +0200

    Improve card style

--- src/shared/Card.component.jsx
+++ src/shared/Card.component.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 
+import { borderColor } from './styles'
+
 const Card = ({ title, children }) => (
-  <fieldset>
-    <legend>{title}</legend>
-    <div style={styles.container}>{children}</div>
+  <fieldset style={styles.container}>
+    <legend style={styles.title}>{title}</legend>
+    <div style={styles.content}>{children}</div>
   </fieldset>
 )
 
 const styles = {
   container: {
+    borderColor,
+    borderWidth: 1,
+  },
+  content: {
     display: 'flex',
+  },
+  title: {
+    fontSize: 12,
+    color: borderColor,
   },
 }
 
