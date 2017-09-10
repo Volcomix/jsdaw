66799854455bf4a81d4ace9e0b1713c2eba26d65
Volcomix <volcomix@gmail.com>
2017-09-10 10:24:21 +0200

    Add shadow transition on synth

--- src/synth/Synth.component.jsx
+++ src/synth/Synth.component.jsx
@@ -4,47 +4,72 @@ import SynthPart from './SynthPart.component'
 import Knob from '../shared/Knob.component'
 import { keyColor, borderColor } from '../shared/styles'
 
-const Synth = ({ name, controls, isSelected, onSelect }) => (
-  <div style={styles.container}>
-    <div
-      style={{
-        ...styles.handle,
-        ...isSelected ? styles.selectedHandle : styles.deselectedHandle,
-      }}
-      onClick={onSelect}
-    >
-    </div>
-    <span
-      style={{
-        ...styles.title,
-        ...isSelected ? styles.selectedTitle : styles.deselectedTitle,
-      }}
-      onClick={onSelect}
-    >
-      {name}
-    </span>
-    {Object.keys(controls).map(key => {
-      const control = controls[key]
-      if (control.value === undefined) {
-        return (
-          <SynthPart
-            key={key}
-            name={toName(key)}
-            controls={control}
-          />
-        )
-      } else {
-        return (
-          <Knob
-            key={key}
-            label={toName(key)}
-            control={control}
-          />
-        )
-      }
-    })}
-  </div>
-)
+class Synth extends React.Component {
+  state = { isSelecting: false }
+
+  render() {
+    const { name, controls, isSelected, onSelect } = this.props
+    const { isSelecting } = this.state
+    return (
+      <div style={{
+        ...styles.container,
+        ...isSelecting ? styles.down : styles.up,
+      }}>
+        <div
+          style={{
+            ...styles.handle,
+            ...isSelected ? styles.selectedHandle : styles.deselectedHandle,
+          }}
+          onClick={onSelect}
+          onMouseDown={this.handleMouseDown}
+          onMouseOut={this.handleMouseUp}
+          onMouseUp={this.handleMouseUp}
+        >
+        </div>
+        <span
+          style={{
+            ...styles.title,
+            ...isSelected ? styles.selectedTitle : styles.deselectedTitle,
+          }}
+          onClick={onSelect}
+          onMouseDown={this.handleMouseDown}
+          onMouseOut={this.handleMouseUp}
+          onMouseUp={this.handleMouseUp}
+        >
+          {name}
+        </span>
+        {Object.keys(controls).map(key => {
+          const control = controls[key]
+          if (control.value === undefined) {
+            return (
+              <SynthPart
+                key={key}
+                name={toName(key)}
+                controls={control}
+              />
+            )
+          } else {
+            return (
+              <Knob
+                key={key}
+                label={toName(key)}
+                control={control}
+              />
+            )
+          }
+        })}
+      </div>
+    )
+  }
+
+  handleMouseDown = () => {
+    this.setState({ isSelecting: true })
+  }
+
+  handleMouseUp = () => {
+    this.setState({ isSelecting: false })
+  }
+}
 
 const styles = {
   container: {
@@ -52,9 +77,15 @@ const styles = {
     position: 'relative',
     alignItems: 'flex-end',
     backgroundColor: 'white',
-    boxShadow: '0 3px 5px rgba(0, 0, 0, 0.3)',
+    transition: 'box-shadow 150ms',
     marginLeft: 8,
     marginBottom: 8,
+  },
+  up: {
+    boxShadow: '0 3px 5px rgba(0, 0, 0, 0.3)',
+  },
+  down: {
+    boxShadow: '0 1px 1px rgba(0, 0, 0, 0.3)',
   },
   handle: {
     alignSelf: 'stretch',
